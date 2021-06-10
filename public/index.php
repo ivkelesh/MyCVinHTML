0fcc3e23acf3ad82c9696e8d6f341b109c8a20e5
Ivan Kelesh <iv.kelesh@yandex.ru>
2021-06-10 09:08:40 -0400

    Added HTML-only version with main info abaut me

---
+++ public/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Curriculum Vitae of Ivan Kelesh</title>
+</head>
+<body>
+	
+			<!-- <h1>Ivan KELESH</h1>
+			<p>Email: iv.kelesh@ya.ru</p>
+			<div>Telefon: 078585889 </div>
+			<div><?php echo "Today is ".date("Y/m/d H:i:s"); ?>	</div>	 -->
+	<header>
+		<h1>Porofolio</h1>
+		<nav>
+			<ul>
+				<li><a href="#about"> About</a></li>
+				<li><a href="#skills">Skills</a></li>
+				<li><a href="#experience">Experience</a></li>
+				<li><a href="#contact">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<main>
+		<section id="about">
+			<h2>About</h2>
+			<img src="" alt="">
+			<p>Hello, i'm Kelesh Ivan! <br> It is my Portofolio page with some interesting info about me, my skills and experiences.			</p>
+		</section>
+
+		<section id="skills">
+			<h2>Skills</h2>
+			<ul>
+				<li>HTML</li>
+				<li>CSS</li>
+				<li>JS</li>
+				<li>Java</li>
+				<li>SQL</li>
+				<li>C/C++</li>
+				<li>Python</li>
+			</ul>
+		</section>
+		
+		<section id="experience">
+			<h2>Experience</h2>
+			<table>
+				<thead>
+					<tr>
+					<th>Period</th>
+					<th>Place</th>
+					<th>Position/Description</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		</section>
+			
+		<section id="contact">
+			<h2>Contact</h2>
+			<ul>
+				<li>Telephone: <a href="">+373 78585889</a></li>
+				<li>Email: iv.kelesh@ya.ru</li>
+				<li>Address: Ceadir-Lunga, str.Street 29</li>
+				<li>Facebook: <a href="www.facebook.com/ivkelesh"></a></li>
+			</ul>
+		</section>
+	
+	
+	</main>
+	<footer>
+		<div align="center">	
+			Copiright &copy 2021, Ivan Kelesh
+		</div>
+	</footer>
+</body>
+</html>
